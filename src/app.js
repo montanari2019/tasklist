106d8af08044eaac94829414d4aa8378622312c7
montanari2019 <ikaro.montanari31@gmail.com>
2021-04-05 21:57:22 -0400

    Primeiro commit configs básicas

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import routes from './routes'
+
+class App {
+    constructor() {
+        this.server = express();
+        this.middleware();
+        this.router();
+    }
+
+    middleware() {
+        this.server.use(express.json());
+    }
+
+    router() {
+        this.server.use(routes);
+    }
+}
+
+export default new App().server;
